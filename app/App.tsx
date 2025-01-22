ce6eda5e27707ce177d44588977668cf11045744
JInsunnya <jsk5754@naver.com>
2025-01-22 19:45:14 +0900

    feat: 회원정보 페이지 관련 작업 및 설정 추가 - babel.config에 plugins 설정 추가, react-native 및 styled-components 설정 추가, App 화면에 회원정보 페이지 이동 버튼 추가, 회원정보 페이지 헤더 UI 작업 진행, 관련 페이지 스타일링 추가

--- app/App.tsx
+++ app/App.tsx
@@ -1,13 +1,43 @@
 import React from 'react';
 import {SafeAreaView, View, Text} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import userProfile from './pages/auth/userProfile';
+import * as A from "./styles/AppStyles";
+
+const Stack = createStackNavigator();
+
+const MainScreen = ({ navigation }) => {
+  return (
+    <A.SafeAreaView>
+      <A.View>
+        <A.Text>App.tsx 페이지</A.Text>
+        <A.Button onPress={() => navigation.navigate('userProfile')}>
+          <A.ButtonText>회원 정보 페이지로 이동</A.ButtonText>
+        </A.Button>
+      </A.View>
+    </A.SafeAreaView>
+  );
+};
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <View>
-        <Text>수연이 슬아 보고 싶당</Text>
-      </View>
-    </SafeAreaView>
+    <A.SafeAreaView>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Main">
+          <Stack.Screen
+            name="Main"
+            component={MainScreen}
+            options={{ title: '메인 화면' }}
+          />
+          <Stack.Screen
+            name="userProfile"
+            component={userProfile}
+            options={{ title: '회원 정보 페이지' }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </A.SafeAreaView>
   );
 };
 
