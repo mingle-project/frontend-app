f7064c73030f67f6afefa7bb1d189c0cd97cca7a
JInsunnya <jsk5754@naver.com>
2025-02-02 21:05:07 +0900

    app.tsx에 소개 페이지 헤더 이동 버튼 추가, introductionHeader 컴포넌트 추가, 관련 UI 스타일 추가

---
+++ app/styles/components/introductionHeaderStyles.ts
@@ -0,0 +1,25 @@
+import styled from 'styled-components/native';
+
+export const SafeAreaView = styled.SafeAreaView`
+  flex: 1;
+  background-color: #f5f5f5;
+`;
+
+export const View = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 45%;
+  margin-top: 15px;
+  margin-right: 35%;
+`;
+
+export const Image1 = styled.Image`
+  width: 30px;
+  height: 25px;
+`;
+
+export const Logo = styled.Image`
+  width: 55px;
+  height: 55px;
+`;
